0efadb9d120ebb73db557c58b4ad93dfbd70d585
Lotanna <sugabelly@gmail.com>
2018-11-15 15:58:19 -0500

    initial

--- Friends/ViewController.swift
+++ Friends/ViewController.swift
@@ -12,7 +12,48 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let greenView = UIView()
+        greenView.translatesAutoresizingMaskIntoConstraints = false
+        greenView.backgroundColor = .green
+        view.addSubview(greenView)
+        
+        let widthConstraint = NSLayoutConstraint(item: greenView,
+                                                 attribute: .width,
+                                                 relatedBy: .equal,
+                                                 toItem: nil,
+                                                 attribute: .notAnAttribute,
+                                                 multiplier: 1.0,
+                                                 constant: 200.0)
+        
+        let heightConstraint = NSLayoutConstraint(item: greenView,
+                                                  attribute: .height,
+                                                  relatedBy: .equal,
+                                                  toItem: greenView,
+                                                  attribute: .width,
+                                                  multiplier: 1.0,
+                                                  constant: 0.0)
+        // greenviewHeight = 2 * greenviewWidth + 0
+        let centerXConstraint = NSLayoutConstraint(item: greenView,
+                                                   attribute: .centerX,
+                                                   relatedBy: .equal,
+                                                   toItem: view,
+                                                   attribute: .centerX,
+                                                   multiplier: 1.0,
+                                                   constant: 0.0)
+        let centerYConstraint = NSLayoutConstraint(item: greenView,
+                                                   attribute: .centerY,
+                                                   relatedBy: .equal,
+                                                   toItem: view,
+                                                   attribute: .centerY,
+                                                   multiplier: 1.0,
+                                                   constant: 0.0)
+        
+        NSLayoutConstraint.activate([widthConstraint, heightConstraint, centerXConstraint, centerYConstraint])
+        
+        
+    }
+    
+
     }
 
 
