a69a6ee38d43e1979ca5fa58ae5aa37be1783ca2
Lotanna <sugabelly@gmail.com>
2019-01-10 17:38:28 -0500

    set up views
    Set up table view and detail view

---
+++ Friends/DetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Friends
+//
+//  Created by Lotanna Igwe-Odunze on 11/15/18.
+//  Copyright © 2018 Lotanna. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+    }
+}
+
